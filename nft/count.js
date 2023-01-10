24610d4d31e8ded14d88c4ce5c3c601f81dc4f20
jokereven <zjing125.cn@gmail.com>
2023-01-10 16:02:10 +0800

    feat: some new follow dog king

---
+++ nft/count.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import { Alchemy, Network } from 'alchemy-sdk'
+import fs from 'fs'
+
+const config = {
+  apiKey: 'KTHY6dSRDoYxkAMQo0EfNEfRBCRvMO1o',
+  network: Network.ETH_MAINNET,
+}
+
+const alchemy = new Alchemy(config)
+
+// 读取json文件
+let rawdata = fs.readFileSync('config.json')
+let parse_rawdata = JSON.parse(rawdata)
+let result = parse_rawdata
+const arr = []
+arr.push(result)
+
+for (let i of arr) {
+  var address = Object.keys(i)
+  for (let index = 0; index < address.length; index++) {
+    var ADDRESS = address[index]
+    const data = await alchemy.core.getTransactionCount(`${ADDRESS}`)
+    var content = `${ADDRESS}\n`
+    const option = {
+      flag: 'a',
+    }
+
+    console.log(data, ADDRESS)
+    if (data > 1980) {
+      fs.writeFile('config.txt', content, option, error => {
+        if (error) {
+          console.log('fs.writeFile failed err: ', error)
+        }
+      })
+    }
+  }
+}
